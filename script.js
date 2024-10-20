53ade6ba496e71146310501a0ea6029e4f5bf659
Georgekuprashvili <kuprashviligeorge@gmail.com>
2024-10-20 19:59:55 +0400

    L14

---
+++ script.js
@@ -0,0 +1,30 @@
+let number = 12;
+let squared = number * number;
+if (squared > 100) {
+  console.log("მეტია ასზე");
+} else {
+  console.log("ნაკლებია ასზე");
+}
+
+let number = 24;
+if ((number > 50, number < 100)) {
+  console.log("მეტია 50 ზე და ნაკლებია 100ზე");
+} else {
+  console.log("რიცხვი არ არის 50დან 100მდე შუალედში");
+}
+
+let number = 2;
+if ((number === 0, number < 0)) {
+  console.log("უდრის ნოლს ან ნაკლებია ნოლზე");
+} else {
+  console.log("მეტია ნულზე");
+}
+
+let length = 10;
+let width = 15;
+let sum = (length + width) * 2;
+if (sum > 50) {
+  console.log("მეტია 50ზე");
+} else {
+  console.log("ნაკლებია 50ზე");
+}
